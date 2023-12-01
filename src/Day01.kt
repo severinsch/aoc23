03f8eb39dff92dfc02c14c93ce1c436dfdebf3eb
sev <severin.schmidmeier@tum.de>
2023-12-01 22:46:27 +0100

    added solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) =
+        input.sumOf { line ->
+            line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+        }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits =
+            (0..9).associateBy { it.toString() } + mapOf (
+            "zero" to 0,
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
+        return input.sumOf {line ->
+            line.findAnyOf(digits.keys)?.let { res -> digits[res.second] }!! * 10 +
+                    line.findLastAnyOf(digits.keys)?.let { res -> digits[res.second] }!!
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput("day01")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
